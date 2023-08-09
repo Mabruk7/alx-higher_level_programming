248840fe665bca94b9aed8a4f80f6fb3c73311ad
Mabruk7 <mabruk33@gmail.com>
2023-08-09 07:13:50 -0700

    updated

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
 /**
- * check_cycle - checks if a linked list contains a cycle
+ * check_cycle - checks if a linked list contains cycle
  * @list: linked list to check
  *
- * Return: 1 if the list has a cycle, 0 if it doesn't
+ * Return: 1 if list contains a cycle, 0 if it doesn't
  */
 int check_cycle(listint_t *list)
 {
@@ -24,3 +24,4 @@ int check_cycle(listint_t *list)
 
 	return (0);
 }
+
