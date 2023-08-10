e5931425f62c5784ccb06bde0fce70074975e98f
Osman Mab-ruk Bawa <122804965+Mabruk7@users.noreply.github.com>
2023-08-10 07:42:05 +0000

    update

--- 0-add.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-a = 1
-b = 2
-
-def add_0(a, b):
-    return a + b
-
-print(f"{a} + {b} = {add_0(a, b)}\n")
--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,7 +2,9 @@
 a = 1
 b = 2
 
+
 def add_0(a, b):
     return a + b
+
 
 print(f"{a} + {b} = {add_0(a, b)}\n")
